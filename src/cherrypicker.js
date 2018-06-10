5923bf904cad63498a6fbe7fdc25cfb13f184b06
MrBBot <me@mrbbot.co.uk>
2018-06-10 19:28:05 +0100

    CLI Stuff

---
+++ src/cherrypicker.js
@@ -0,0 +1,40 @@
+const cheerio = require("cheerio");
+const css = require("css");
+
+function checkSelector(selector, htmlFiles) {
+    let pseudoIndex = selector.indexOf(":");
+    if (pseudoIndex > -1) selector = selector.substring(0, pseudoIndex);
+
+    for (let htmlFile of htmlFiles) if (htmlFile.$(selector).length > 0) return true;
+    return false;
+}
+
+function cherrypickNode(node, htmlFiles) {
+    let removedRules = 0;
+    node.rules = node.rules.filter(rule => {
+        if ("selectors" in rule) {
+            rule.selectors = rule.selectors.filter(selector => checkSelector(selector, htmlFiles));
+            if(rule.selectors.length === 0) removedRules++;
+            return rule.selectors.length !== 0;
+        } else if ("rules" in rule) {
+            removedRules += cherrypickNode(rule, htmlFiles);
+            return rule.rules.length !== 0;
+        }
+    });
+    return removedRules;
+}
+
+function cherrypick(htmlFiles, cssFiles) {
+    htmlFiles.forEach(htmlFile => htmlFile.$ = cheerio.load(htmlFile.content));
+    cssFiles.forEach(cssFile => {
+        cssFile.stylesheet = css.parse(cssFile.content);
+        // noinspection JSUnresolvedVariable
+        cssFile.removedRules = cherrypickNode(cssFile.stylesheet.stylesheet, htmlFiles);
+        cssFile.output = css.stringify(cssFile.stylesheet);
+    });
+    return cssFiles;
+}
+
+module.exports = {
+    cherrypick
+};
---
+++ src/cli.js
@@ -0,0 +1,41 @@
+const cli = require("cli").enable("glob");
+const fs = require("fs");
+const path = require("path");
+const {filter, reduce} = require("asyncro");
+const {cherrypick} = require("./cherrypicker");
+
+const promisify = f => (...args) => new Promise((resolve, reject) =>
+    f.apply(null, [...args, (err, result) => err ? reject(err) : resolve(result)]));
+
+const stats = promisify(fs.stat);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const glob = promisify(cli.glob);
+
+const cwd = process.cwd();
+
+(async function () {
+    let argFilePaths = await reduce(cli.args, async (argFilePaths, arg) => argFilePaths.concat(await glob(arg)), []);
+    argFilePaths = await filter(argFilePaths, async (argFile) => !((await stats(argFile)).isDirectory()));
+
+    const files = await reduce(argFilePaths, async (files, filePath) => {
+        const ext = filePath.substring(filePath.lastIndexOf(".") + 1);
+        files[ext] = files[ext] || [];
+        files[ext].push({
+            path: filePath,
+            content: await readFile(filePath, {encoding: "utf-8"}),
+        });
+        return files;
+    }, {});
+    console.log(files);
+
+    const cherrypickedCssFiles = cherrypick(files["html"] || [], files["css"] || []);
+
+    //TODO: Make all necessary dirs for output
+    for(let cssFile of cherrypickedCssFiles) {
+        cssFile.cherrypickedPath = path.resolve("cherrypicked" + path.resolve(cssFile.path).substring(cwd.length));
+        await writeFile(cssFile.cherrypickedPath, cssFile.output, {encoding: "utf-8"});
+    }
+
+    console.log(cherrypickedCssFiles);
+})();
